aa219f2bff1882667822a6359417dfc9584fee7a
Benji <benkosa0@gmail.com>
2022-11-18 20:15:04 +0100

    add response and opertions file

---
+++ aus/subory/src/main/java/org/main/app/Operations.java
@@ -0,0 +1,56 @@
+package org.main.app;
+
+
+import org.main.shared.Response;
+
+public class Operations {
+    /**
+     * 1. Vyhľadanie záznamov pacienta (identifikovaný svojím rodným číslom).
+     * Po nájdení pacienta je potrebné zobraziť všetky evidované údaje vrátane
+     * všetkých hospitalizácií.
+     */
+    Response op1() {
+        return null;
+    }
+    /**
+     * 2. Vyhľadanie hospitalizácie (definovaná id hospitalizácie) pre pacienta (definovaný
+     * rodným číslom) azobrazenie všetkých údajov o nej. (Kľúč, ktorý pôjde do
+     */
+    Response op2() {
+        return null;
+    }
+    /**
+     * 3. Vykonanie záznamu o začiatku hospitalizácie pacienta (identifikovaný svojím rodným
+     * číslom).
+     */
+    Response op3() {
+        return null;
+    }
+    /**
+     * 4. Vykonanie záznamu o ukončení hospitalizácie pacienta (identifikovaný svojím rodným
+     * číslom).
+     */
+    Response op4() {
+        return null;
+    }
+    /**
+     * 5. Pridanie pacienta.
+     */
+    Response op5() {
+        return null;
+    }
+    /**
+     * 6. Vymazanie hospitalizácie (definovaná id hospitalizácie) pre pacienta (definovaný
+     * rodným číslom).
+     */
+    Response op6() {
+        return null;
+    }
+    /**
+     * 7. Vymazanie pacienta.
+     */
+    Response op7() {
+        return null;
+    }
+
+}
