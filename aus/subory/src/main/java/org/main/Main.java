23a765614e4ac65b037422abf7c6ac1ef4157b8a
Benji <benkosa0@gmail.com>
2022-11-19 22:42:47 +0100

    menu

--- aus/subory/src/main/java/org/main/Main.java
+++ aus/subory/src/main/java/org/main/Main.java
@@ -13,27 +13,27 @@ public class Main {
 
         gui.start();
 
-        Tests tests = new Tests();
-        tests.testSize();
-        tests.testRandomOperation(
-                .5,
-                .25,
-                .25,
-                10,
-                100,
-                100,
-                10,
-                10000
-        );
-
-        Hashing<Pacient> test = new Hashing<>(
-                "file.dat",
-                10,
-                10,
-                Pacient.class
-        );
-
-        test.readWholeFileNoValid();
+//        Tests tests = new Tests();
+//        tests.testSize();
+//        tests.testRandomOperation(
+//                .5,
+//                .25,
+//                .25,
+//                10,
+//                100,
+//                100,
+//                10,
+//                10000
+//        );
+//
+//        Hashing<Pacient> test = new Hashing<>(
+//                "file.dat",
+//                10,
+//                10,
+//                Pacient.class
+//        );
+//
+//        test.readWholeFileNoValid();
 
 
     }
