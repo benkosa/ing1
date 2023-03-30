ccd55bdca479b0d217e47084ced19b48540870d4
Benjamín Kosa <benkosa0@gmail.com>
2023-03-30 10:31:10 +0200

    fix

--- diss/_1proj/src/main/java/org/main/_2zadanie/VehicleInspectionEvent.java
+++ diss/_1proj/src/main/java/org/main/_2zadanie/VehicleInspectionEvent.java
@@ -12,6 +12,25 @@ public class VehicleInspectionEvent extends VehicleEvent{
             stk.group2.hireWorker();
             stk.scheduleStartInspection(newVehicle);
         }
+        // ak je niekto v rade na zaplatenie
+        // a je volny dalsi zamestnanec zo skupiny 1
+        if (stk.queueAfterStk.getSize() > 0 && stk.group1.isWorkerFree()) {
+            final Vehicle newVehicle = stk.queueAfterStk.poll();
+            stk.group1.hireWorker();
+            stk.scheduleStartPayment(newVehicle);
+            // ak je auto v rade pred stk
+            // a je miesto v rade na konrolu
+            // a je volny dalsi zamestnanec zo skupiny 1
+        } else if (
+                stk.queueBeforeStk.getSize() > 0 &&
+                        stk.isSpaceInsideStk() &&
+                        stk.group1.isWorkerFree()
+        ) {
+            final Vehicle newVehicle = stk.queueBeforeStk.poll();
+            stk.group1.hireWorker();
+            stk.arrivedInStkQueue(newVehicle);
+            stk.scheduleReceiveVehicle(newVehicle);
+        }
     }
 
     public VehicleInspectionEvent(double eventTime, EventSimulationCore myCore, Vehicle vehicle) {
