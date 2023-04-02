c08c6d684925022cdb497287601d38ca5fb78c39
Benjamín Kosa <benkosa0@gmail.com>
2023-04-02 12:09:19 +0200

    refactor

--- diss/_1proj/src/main/java/org/main/_2zadanie/STK.java
+++ diss/_1proj/src/main/java/org/main/_2zadanie/STK.java
@@ -188,9 +188,4 @@ public class STK extends EventSimulationCore {
         averageQueueBeforeSTK.countResult();
         initialize();
     }
-
-    public void changeWorkers(int w1, int w2) {
-        group1 = new WorkersGroup(w1);
-        group2 = new WorkersGroup(w2);
-    }
 }
