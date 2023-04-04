f68da58acba1c66e22dfd8d2cee03b15a4a0b89f
Benjamín Kosa <benkosa0@gmail.com>
2023-04-04 10:43:40 +0200

    vehicle arrived event adjust

--- diss/_1proj/src/main/java/org/main/_2zadanie/VehicleArrivedEvent.java
+++ diss/_1proj/src/main/java/org/main/_2zadanie/VehicleArrivedEvent.java
@@ -8,15 +8,26 @@ public class VehicleArrivedEvent extends VehicleEvent{
         stk.scheduleNewArrival();
         stk.saveArrivedVehicle(vehicle);
         vehicle.setArrived(stk.getCurrentTime());
-        // ak je volny woker a je miesto v rade na kontrolu
-        if (stk.group1.isWorkerFree() && stk.isSpaceInsideStk()) {
-            vehicle.startWaitingInQue = stk.getCurrentTime();
-            stk.averageWaitingBeforeSTK.countAverageTimeInQueue(vehicle.startWaitingInQue);
-            stk.group1.hireWorker(this.vehicle);
-            stk.arrivedInStkQueue(this.vehicle);
-            stk.scheduleReceiveVehicle(this.vehicle);
-            stk.averageQueueBeforeSTK.countAverageQueueLength();
-        // vojdeme do radu pred stk
+
+        // ak je niekto v rade na zaplatenie
+        // a je volny dalsi zamestnanec zo skupiny 1
+        if (stk.queueAfterStk.getSize() > 0 && stk.group1.isWorkerFree()) {
+            final Vehicle newVehicle = stk.queueAfterStk.poll();
+            stk.group1.hireWorker(newVehicle);
+            stk.scheduleStartPayment(newVehicle);
+            System.out.println(newVehicle.getStartWaitingInQue());
+            // ak je auto v rade pred stk
+            // a je miesto v rade na konrolu
+            // a je volny dalsi zamestnanec zo skupiny 1
+        } else if (
+                stk.queueBeforeStk.getSize() > 0 &&
+                        stk.isSpaceInsideStk() &&
+                        stk.group1.isWorkerFree()
+        ) {
+            final Vehicle newVehicle = stk.queueBeforeStk.poll();
+            stk.group1.hireWorker(newVehicle);
+            stk.arrivedInStkQueue(newVehicle);
+            stk.scheduleReceiveVehicle(newVehicle);
         } else {
             vehicle.arrivedInQueue(stk.getCurrentTime());
             stk.queueBeforeStk.addQueue(vehicle);
