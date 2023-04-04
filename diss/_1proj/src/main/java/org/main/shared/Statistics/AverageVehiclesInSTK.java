70171c5105bc4c28219e7ce9f1a0e5526988138b
Benjamín Kosa <benkosa0@gmail.com>
2023-04-04 22:06:39 +0200

    input test

--- diss/_1proj/src/main/java/org/main/shared/Statistics/AverageVehiclesInSTK.java
+++ diss/_1proj/src/main/java/org/main/shared/Statistics/AverageVehiclesInSTK.java
@@ -3,12 +3,22 @@ public class AverageVehiclesInSTK extends Statistics {
     private long vehicleArrived;
     private long vehicleLeft;
 
+    private double totalArrived;
+    private double totalLeft;
+
     @Override
     protected double replicationResult() {
         if (vehicleLeft > vehicleArrived) {
             System.out.println("warning: more vehicles left than arrived");
         }
+        totalArrived += vehicleArrived;
+        totalLeft += vehicleLeft;
         return (double)vehicleArrived - vehicleLeft;
+    }
+
+    public void testTotalInOut() {
+        System.out.println(totalArrived/getCountReplications());
+        System.out.println(totalLeft/getCountReplications());
     }
 
     @Override
--- diss/_1proj/src/main/java/org/main/shared/Statistics/Statistics.java
+++ diss/_1proj/src/main/java/org/main/shared/Statistics/Statistics.java
@@ -1,8 +1,13 @@
 package org.main.shared.Statistics;
 
 public abstract class Statistics {
-    private long countReplications = 0;
+    private double countReplications = 0;
     private double sumResults = 0;
+
+    public double getCountReplications() {
+        return countReplications;
+    }
+
     protected abstract double replicationResult();
     public double totalResult() {
         return sumResults / countReplications;
