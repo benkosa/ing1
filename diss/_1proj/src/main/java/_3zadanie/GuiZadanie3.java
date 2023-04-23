a4c97b9fd6634bf4acd513986c3b338e08abfff3
Benjamín Kosa <benkosa0@gmail.com>
2023-04-23 16:56:34 +0200

    added gui

---
+++ diss/_1proj/src/main/java/_3zadanie/GuiZadanie3.java
@@ -0,0 +1,49 @@
+package _3zadanie;
+
+import javax.swing.*;
+
+public class GuiZadanie3 {
+    private JPanel panel1;
+    private JScrollPane ScrollPane1;
+    private JScrollPane ScrollPane2;
+    private JScrollPane ScrollPane4;
+    private JScrollPane ScrollPane5;
+    private JLabel count1;
+    private JLabel count2;
+    private JLabel count3;
+    private JLabel count4;
+    private JLabel count5;
+    private JScrollPane ScrollPane6;
+    private JScrollPane ScrollPane3;
+    private JLabel count6;
+    private JButton startRealTimeButton;
+    private JButton startTurboButton;
+    private JTextField a100000TextField;
+    private JTextField a0TextField;
+    private JTextField a60TextField;
+    private JTextField a1000TextField;
+    private JTextField a5TextField;
+    private JTextField a20TextField;
+    private JButton adjustSlowDownButton;
+    private JButton graph2Button;
+    private JButton pauseButton;
+    private JButton playButton;
+    private JButton graph1Button;
+    private JButton a2000Button;
+    private JButton a1000Button;
+    private JButton a10Button;
+    private JButton a100Button;
+    private JButton stopButton;
+    private JScrollPane ScrollPane7;
+    private JScrollPane ScrollPane8;
+    private JLabel g1nuCount;
+    private JLabel g1uCount;
+    private JScrollPane ScrollPane9;
+    private JScrollPane ScrollPane10;
+    private JLabel g2uCount;
+    private JLabel g2nuCount;
+    private JLabel simTime;
+    private JLabel lastEvent;
+    private JLabel realTime;
+    private JTextArea textArea1;
+}
